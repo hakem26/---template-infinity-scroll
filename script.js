66052f64bbaacd033f7814e7fc9ea5d7641c1fc2
hakem26 <hamed26km@gmail.com>
2024-01-03 17:04:57 +0330

    finish

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 // 1 >> GI ELEMENTS
-const loader = document.getElementById('loader');
+const loaderFirst = document.getElementById('loader-first');
+const loaderMore = document.getElementById('loader-more');
 const imgGallery = document.getElementById('img-gallery');
 
 // 2 >> VARIABLES
+let ready = false
+let imagesLoaded = 0
+let totalImages = 0
 let photosArray = []
-let count = 15
 
 // 3 >> API CONSTS
+let count = 30
 const apiKey = 'xZNSVx0q1W3ieDPbpZ3wPDgZTSBs6e1hwDU7qDfi580'
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`
 
@@ -17,8 +21,21 @@ function setAttributes(element, attribute) {
     }
 }
 
-// 6 >> FUNCTION -> CREATE ELEMENTS FOR LINKS, PHOTOS, ADD TO DOM
+// 7 >> FUNCTION -> CHECK IF ALL IMAGES IS LOADED
+function imageLoaded() {
+    imagesLoaded++
+    if (imagesLoaded === totalImages) {
+        ready = true
+        loaderFirst.hidden = true
+    }
+}
+
+// 5 >> FUNCTION -> CREATE ELEMENTS FOR LINKS, PHOTOS, ADD TO DOM
 function displayPhotos() {
+    //7
+    imagesLoaded = 0
+    totalImages = photosArray.length
+    //5
     photosArray.forEach(photo => {
         const item = document.createElement('a')
         setAttributes(item, {
@@ -36,6 +53,9 @@ function displayPhotos() {
             alt: photo.alt_description,
             title: photo.alt_description
         })
+        //7
+        img.addEventListener('load', imageLoaded);
+        //5
         imgWrap.appendChild(img)
         item.appendChild(imgWrap)
         imgGallery.appendChild(item)
@@ -52,6 +72,14 @@ async function getPhotos() {
         throw error
     }
 }
+
+// 8 >> LOAD IMAGES NEAR SCROLLING FROM BOTTOM OF SCREEN
+window.addEventListener('scroll', () => {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
+        ready = false;
+        getPhotos();
+    }
+});
 
 // 5 >> ON-LOAD
 getPhotos()
